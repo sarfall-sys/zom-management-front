086ab06d2d06762e132badf79d3b2fca577e4b64
sarfall-sys <sarfall00@gmail.com>
2026-01-28 00:03:23 -0500

    Refactor ChartCard component to support both pie and bar chart types; update AdminDashboard to utilize new ChartCard functionality

--- src/components/common/ChartCard.jsx
+++ src/components/common/ChartCard.jsx
@@ -1,18 +1,46 @@
-import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from "recharts";
+import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer ,Pie, PieChart, TooltipIndex } from "recharts";
 
-function ChartCard({data}) {
-  return (
-    <>
-      <ResponsiveContainer width="100%" height={300}>
-        <BarChart data={data}>
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Bar dataKey="value" fill="#8884d8" />
-        </BarChart>
-      </ResponsiveContainer>
-    </>
-  )
+function ChartCard({title, data,chartType}) {
+
+  if(chartType==="pie"){
+    return ( 
+      <>
+        <h3 className="text-lg font-medium mb-4">{title}</h3>
+        <ResponsiveContainer width="100%" height={300}>
+          <PieChart>
+            <Pie
+              data={data}
+              dataKey="value"
+              nameKey="name"
+              cx="50%"
+              cy="50%"
+              outerRadius={100}
+              fill="#8884d8"
+              label
+            />
+            <Tooltip />
+          </PieChart>
+        </ResponsiveContainer>
+      </>
+    )
+  }
+
+  if(chartType==="bar"){
+    return (
+      <>
+        <h3 className="text-lg font-medium mb-4">{title}</h3>
+        <ResponsiveContainer width="100%" height={300}>
+          <BarChart data={data}>
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Bar dataKey="value" fill="#8884d8" />
+          </BarChart>
+        </ResponsiveContainer>
+      </>
+    )
+  }
+  return null;
 }
 
 export default ChartCard
--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 
+import DashboardCard from '../../components/common/DashboardCard'
+import ChartCard from '../../components/common/ChartCard'
+import StatsCard from '../../components/common/StatsCard'
 function AdminDashboard() {
   return (
     <>
-    
-    <section>
-      <div>Admin Dashboard</div>
+    <section className="container mx-auto p-4 ">
+      <h1 className="text-3xl font-bold mb-6">Admin Dashboard</h1>
+
+    <DashboardCard title="Statistics Overview">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <StatsCard title="Total Users" value="1,234" />
+        <StatsCard title="Total Orders" value="567" />
+        <StatsCard title="Total Revenue" value="$89,012" />
+      </div>
+      <div>
+        <ChartCard title="User Growth Over Time" chartType="line" />
+      </div>
+      <div>
+        <ChartCard title="Revenue Breakdown" chartType="pie" />
+      </div>
+    </DashboardCard>
+
+
 
       
 
